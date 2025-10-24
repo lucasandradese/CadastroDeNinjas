a7dd6f113de02d90109daabbe7acdf41036ed925
lucasandradese <lucasandradese@outlook.com>
2025-10-23 22:39:18 -0300

    feat: Cria MissoesDTO e MissoesMapper como forma de proteger o MissoesModel e colocar logica para entidade missao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,5 +1,63 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class MissoesService {
 
+    private final MissoesMapper missoesMapper;
+    private final MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesMapper missoesMapper, MissoesRepository missoesRepository) {
+        this.missoesMapper = missoesMapper;
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Lista todas as missoes
+    public List<MissoesDTO> listarMissoes(){
+        List<MissoesModel> missoes = missoesRepository.findAll();
+
+        return missoes.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    // Lista a missão por ID
+    public MissoesDTO listarMissoesId(Long id){
+        Optional<MissoesModel> missoesId = missoesRepository.findById(id);
+        return missoesId.map(missoesMapper::map).orElse(null);
+    }
+
+    // Criar missoes
+    public MissoesDTO criarMissao(MissoesDTO missoesDTO){
+        MissoesModel missao = missoesMapper.map(missoesDTO);
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao);
+    }
+
+    // Deletar missoes
+    public void delete(Long id){
+        missoesRepository.deleteById(id);
+    }
+    // Atualizar missão
+    public MissoesDTO missoesAlterar(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missao = missoesRepository.findById(id);
+        if(missao.isPresent()){
+            MissoesModel missaoExistente = missao.get();
+
+            if(missoesDTO.getNome() != null){
+                missaoExistente.setNome(missoesDTO.getNome());
+            }
+            if (missoesDTO.getDificuldade() != null){
+                missaoExistente.setDificuldade(missoesDTO.getDificuldade());
+            }
+            MissoesModel missoesSalvo = missoesRepository.save(missaoExistente);
+            return missoesMapper.map(missoesSalvo);
+        }
+        return null;
+    }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -7,10 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -54,4 +52,11 @@ public class NinjaControllerUi {
         model.addAttribute("ninja", new NinjaDTO());
         return "cadastroninja";
     }
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes) {
+        ninjaService.criarNinja(ninja);
+        redirectAttributes.addFlashAttribute("mensagem", "Ninja cadastrado com sucesso!");
+        return "redirect:/ninjas/ui/listar";
+    }
+
 }
